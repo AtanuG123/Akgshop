03b8b84ee731c70ba8f78e9cdcaf7debdc116981
AtanuG123 <109225904+AtanuG123@users.noreply.github.com>
2024-04-03 21:08:27 +0530

    9th

--- akgshopp/src/component/crousal.jsx
+++ akgshopp/src/component/crousal.jsx
@@ -6,15 +6,15 @@ import React from "react";
 
 import img1 from "../assets/sale1.jpg";
 import img2 from "../assets/sale2.jpg";
-import img3 from "../assets/sale3.jpg";
-import img4 from "../assets/ad.jpg";
+// import img3 from "../assets/sale3.jpg";
+// import img4 from "../assets/ad.jpg";
 export default function Crousal() {
-  const images=[img1,img2,img3,img4]
+  // const images=[img1,img2,img3,img4]
   
   
   return (
     <div className="homeback">
-      <img  src={images[1]}></img>
+      <img  src={img2}></img>
     </div>
   );
 }
--- akgshopp/src/pagesjs/home.jsx
+++ akgshopp/src/pagesjs/home.jsx
@@ -1,14 +1,16 @@
 import Crousal from "../component/crousal";
 import Navber from "../component/navber";
 import "../pagescss/home.css";
-import img1 from "../assets/sale1.jpg";
-import img2 from "../assets/sale2.jpg";
-import img3 from "../assets/footvalue.jpg";
+import supersakti from "../assets/supersakti.jpg";
+import chupri from "../assets/chupri.jpg";
+import tajbucket from "../assets/tajbucket.jpg"
+import lowbucket from "../assets/lowbucket.jpg"
+import balti from "../assets/balti.jpg";
+import salt from "../assets/salt.jpg";
+import chapa  from "../assets/chapa.jpg";
+import telfish from "../assets/telfish.jpg";
 import ecoflex from "../assets/eco flex.jpg";
-import pipe from "../assets/pipe.jpeg";
-import balti from "../assets/balti.png";
-import bucket3 from "../assets/bucket3.jpg";
-import telfish from "../assets/telfish.png";
+
 export default function Home() {
   return (
     <div>
@@ -22,7 +24,7 @@ export default function Home() {
           <li>
             <img
               onClick={() => switchonclick("man")}
-              src={pipe}
+              src={supersakti}
               alt="loading"
             ></img>
             <div onClick={() => switchonclick("man")} className="seedetails">
@@ -37,7 +39,7 @@ export default function Home() {
           <li>
             <img
               onClick={() => switchonclick("woman")}
-              src={bucket3}
+              src={chupri}
               alt="loading"
             ></img>
             <div onClick={() => switchonclick("woman")} className="seedetails">
@@ -53,31 +55,59 @@ export default function Home() {
           <li>
             <img
               onClick={() => switchonclick("style")}
-              src={bucket3}
+              src={tajbucket}
               alt="loading"
             ></img>
             <div onClick={() => switchonclick("style")} className="seedetails">
-              <h3>Plastic Bucket</h3>
+              <h3>Plastic Taj Bucket</h3>
               <ul>
-                <li>Taj 14 inch </li>
-                <li>14 inch local </li>
-                <li>16 inch local </li>
+                <li>14 inch </li>
+                <li>16 inch </li>
               </ul>
             </div>
           </li>
-
           <li>
             <img
-              onClick={() => switchonclick("man")}
-              src={ecoflex}
+              onClick={() => switchonclick("style")}
+              src={lowbucket}
               alt="loading"
             ></img>
-            <div onClick={() => switchonclick("man")} className="seedetails">
-              <h3>Pankaj Super flex suction</h3>
+            <div onClick={() => switchonclick("style")} className="seedetails">
+              <h3>Plastic local Bucket</h3>
               <ul>
-              <li>2 inch 60 MM</li>
-                <li>2.5 inch 65 MM</li>
-                <li>3 inch 70 MM</li>
+                
+                <li>14 inch</li>
+                <li>16 inch</li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <img
+              onClick={() => switchonclick("style")}
+              src={salt}
+              alt="loading"
+            ></img>
+            <div onClick={() => switchonclick("style")} className="seedetails">
+              <h3>Salt </h3>
+              <ul>
+                <li>25 kg Bag</li>
+                <li>1 kg packet</li>
+                <li>500g packet</li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <img
+              onClick={() => switchonclick("style")}
+              src={chapa}
+              alt="loading"
+            ></img>
+            <div onClick={() => switchonclick("style")} className="seedetails">
+              <h3>Chapa</h3>
+              <ul>
+                <li>single size</li>
+                {/* <li>1kg packet</li>
+                <li>500g packet</li> */}
               </ul>
             </div>
           </li>
@@ -99,6 +129,21 @@ export default function Home() {
           </li>
           <li>
             <img
+              onClick={() => switchonclick("man")}
+              src={ecoflex}
+              alt="loading"
+            ></img>
+            <div onClick={() => switchonclick("man")} className="seedetails">
+              <h3>Pankaj Super flex suction</h3>
+              <ul>
+              <li>2 inch 60 MM</li>
+                <li>2.5 inch 65 MM</li>
+                <li>3 inch 70 MM</li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <img
               onClick={() => switchonclick("style")}
               src={balti}
               alt="loading"
@@ -112,36 +157,8 @@ export default function Home() {
               </ul>
             </div>
           </li>
-          <li>
-            <img
-              onClick={() => switchonclick("style")}
-              src={balti}
-              alt="loading"
-            ></img>
-            <div onClick={() => switchonclick("style")} className="seedetails">
-              <h3>Salt </h3>
-              <ul>
-                <li>25 kg Bag</li>
-                <li>1 kg packet</li>
-                <li>500g packet</li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <img
-              onClick={() => switchonclick("style")}
-              src={balti}
-              alt="loading"
-            ></img>
-            <div onClick={() => switchonclick("style")} className="seedetails">
-              <h3>Chapa</h3>
-              <ul>
-                <li>single size</li>
-                {/* <li>1kg packet</li>
-                <li>500g packet</li> */}
-              </ul>
-            </div>
-          </li>
+          
+          
         </ul>
       </div>
     </div>
